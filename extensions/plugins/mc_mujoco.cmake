07f160e06ff8c19da89ba5bc8888e334b8dc1e08
Alice CARIOU <cariou.alice@protonmail.com>
2026-01-30 11:07:43 +0900

    fix: mc_mujoco remote & branch

--- extensions/plugins/mc_mujoco.cmake
+++ extensions/plugins/mc_mujoco.cmake
@@ -1,5 +1,6 @@
 AddProject(mc_mujoco
-  GITHUB mathieu-celerier/mc_mujoco
-  GIT_TAG origin/main
+	#GITHUB mathieu-celerier/mc_mujoco
+  GITHUB bastien-muraccioli/mc_mujoco
+  GIT_TAG origin/safe-rl-qp
   DEPENDS mc_rtc
 )
